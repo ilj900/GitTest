fe7bf30cc5c8d8ce8c4eb71725ca73eb1439b9a6
Roman <ilj900@gmail.com>
2022-06-03 12:22:49 +0300

    B3

--- main.cpp
+++ main.cpp
@@ -3,5 +3,5 @@
 void main()
 {
 	std::cout<<"A Brand New \" Hello World\""<<std::endl;
-	std::cout<<"With Some New \" Functionality\""<<std::endl;
+	std::cout<<"With Another, Another, Brand New \" Functionality\""<<std::endl;
 }
